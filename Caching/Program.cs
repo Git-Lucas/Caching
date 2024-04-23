7e1411b3960cc085a61ac8d3394a8a0aed0b583e
Git-Lucas <lucas.oliver_2000@hotmail.com>
2024-04-23 16:44:01 -0300

    [C] - feat: Adicionado cache em memória no endpoint que solicita as previsões de tempo (5 segundos de inatividade para remover do cache)

--- Caching/Program.cs
+++ Caching/Program.cs
@@ -1,13 +1,15 @@
+using Caching.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,24 +23,29 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+string nameEndpoint = "GetWeatherForecast";
+app.MapGet("/weatherforecast", ([FromServices]IMemoryCache memoryCache) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
+    if (!memoryCache.TryGetValue(nameEndpoint, out WeatherForecast[]? forecast))
+    {
+        forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+
+        MemoryCacheEntryOptions cacheEntryOptions = new MemoryCacheEntryOptions()
+            .SetSlidingExpiration(TimeSpan.FromSeconds(5));
+
+        memoryCache.Set(nameEndpoint, forecast, cacheEntryOptions);
+    }
+
     return forecast;
 })
-.WithName("GetWeatherForecast")
+.WithName(nameEndpoint)
 .WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
